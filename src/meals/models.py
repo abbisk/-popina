38d637bc608c57cbc9d6665dc48db1f624ee9766
abbisk <abbiskpro@gmail.com>
2020-06-30 22:55:59 +0530

    Updated requirements file

--- src/meals/models.py
+++ src/meals/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Meals(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=500)
+    people = models.IntegerField()
+    price = models.DecimalField(max_digits=5)
+    preperation_time = models.IntegerField()
+    image = models.ImageField(upload_to='meals/')
