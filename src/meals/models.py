a685cfbe148ef4cb5d1fbdb25b64875d0710ceb1
abbisk <abbiskpro@gmail.com>
2020-07-06 00:18:39 +0530

    Update settings and urls to manage static files.

--- src/meals/models.py
+++ src/meals/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.text import slugify
 
 # Create your models here.
 class Meals(models.Model):
@@ -8,7 +9,16 @@ class Meals(models.Model):
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preperation_time = models.IntegerField()
     image = models.ImageField(upload_to='meals/')
+    slug = models.SlugField(blank=True, null=False)
 
+    def save(self , *args , **kwargs):
+        if not self.slug and self.name :
+            self.slug = slugify(self.name)
+        super(Meals , self).save(*args , **kwargs)
+
+    class Meta:
+        verbose_name = 'meal'
+        verbose_name_plural = 'meals'
 
     def __str__ (self):
         return self.name
