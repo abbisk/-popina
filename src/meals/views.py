7fdb02cf005f3f63b20d29a05abcdda8422d05ea
abbisk <abbiskpro@gmail.com>
2020-07-07 21:59:12 +0530

    Added Meals List View

--- src/meals/views.py
+++ src/meals/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+from .models import Meals
+
+def meal_list(request):
+    meal_list = Meals.objects.all()
+
+    context = {'meal_list' : meal_list ,}
+
+    return render(request , 'Meals/list.html' , context)
+
+def meal_detail(request , slug):
+    pass
