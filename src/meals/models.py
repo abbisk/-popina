6a1307cc13b9f99614bfb56b38898efd20fd7240
abbisk <abbiskpro@gmail.com>
2021-01-09 21:38:15 +0530

    Added Category

--- src/meals/models.py
+++ src/meals/models.py
@@ -5,6 +5,7 @@ from django.utils.text import slugify
 class Meals(models.Model):
     name = models.CharField(max_length=50)
     description = models.TextField(max_length=500)
+    category = models.ForeignKey('Category', on_delete= models.SET_NULL, null=True)
     people = models.IntegerField()
     price = models.DecimalField(max_digits=5, decimal_places=2)
     preperation_time = models.IntegerField()
@@ -22,3 +23,15 @@ class Meals(models.Model):
 
     def __str__ (self):
         return self.name
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=30)
+
+    class Meta:
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+    def __str__(self):
+        return self.name
+    
